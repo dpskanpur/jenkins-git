37e70bd6546530bfe30e0f974a22b7648e58cad4
Alok Singh <63167832+dpskanpur@users.noreply.github.com>
2022-08-25 16:10:31 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -3,7 +3,7 @@ locals {
   ip_cidr_range          = "192.168.0.0/16"
   ip_cidr_range_secondry = "10.0.0.0/16"
   region                 = "us-central1"
-  range_name             = "network-range"
+  range_name             = "network-range-dps"
   bool                   = "false"
 }
 provider "google" {
